c5145d8d064fde528c4cf16ff1ab2f188d1223f1
Dolfas <rodolfo.amorim@tecnico.ulisboa.pt>
2023-12-15 17:20:52 +0000

    CORRECTION to code 1

--- Homework 1/hw1-q1.py
+++ Homework 1/hw1-q1.py
@@ -47,12 +47,14 @@ class Perceptron(LinearModel):
         other arguments are ignored
         """
         # Q1.1a
-        y_predicted = np.argmax(self.W.dot(x_i))
-        if y_predicted != y_i:
-            # Perceptron update
+        y_predicted = np.argmax(self.W.dot(x_i)) #Perceptron prediction 
+
+        if y_predicted != y_i: # Perceptron weigth update
+            
             self.W[y_i, :] +=  x_i
             self. W[y_predicted, :] -= x_i
-        #raise NotImplementedError
+
+        
 
 
 class LogisticRegression(LinearModel):
@@ -63,40 +65,42 @@ class LogisticRegression(LinearModel):
         learning_rate (float): keep it at the default value for your plots
         """
         # Q1.1b
-        # Get probability scores according to the model (num_labels x 1).
-        label_scores = np.expand_dims(self.W.dot(x_i), axis = 1)
+        # Get the scores for each label, in order to do softmax later
+        scores = np.expand_dims(self.W.dot(x_i), axis = 1)
 
-        # One-hot encode true label (num_labels x 1).
+        # One-hot encoding 
         y_one_hot = np.zeros((np.size(self.W, 0),1))
         y_one_hot[y_i] = 1
 
-        # Softmax function
-        # This gives the label probabilities according to the model (num_labels x 1).
-        label_probabilities = np.exp(label_scores) / np.sum(np.exp(label_scores))
+        # Softmax function, using previous scores for the labels
+        probs = np.exp(scores) / np.sum(np.exp(scores))
         
-        # SGD update. W is num_labels x num_features.
-        self.W = self.W + learning_rate * (y_one_hot - label_probabilities).dot(np.expand_dims(x_i, axis = 1).T)
-        #raise NotImplementedError
+        # Stochastic gradient descent update
+        self.W = self.W + learning_rate * (y_one_hot - probs).dot(np.expand_dims(x_i, axis = 1).T)
 
 
 class MLP(object):
     # Q3.2b. This MLP skeleton code allows the MLP to be used in place of the
     # linear models with no changes to the training loop or evaluation code
     # in main().
+
     def __init__(self, n_classes, n_features, hidden_size):
         # Initialize an MLP with a single hidden layer.
         #the number of hidden units in the hidden layer was set to 200 as per the question 1.2.2 information
         self.hidden = []
+
         units = [n_features, 200, n_classes]
+
+        #Weigths Initialization
         W1 = np.random.normal(loc=0.1,scale=0.1,size=(units[1], units[0]))
         W2 = np.random.normal(loc=0.1,scale=0.1,size=(units[2], units[1]))
         
+        #Bias Initialization
         b1 = np.zeros((units[1],1))
         b2 = np.zeros((units[2],1))
 
         self.weights = [W1, W2]
         self.biases = [b1, b2]        
-        #raise NotImplementedError
 
     def predict(self, X):
         # Compute the forward pass of the network. At prediction time, there is
@@ -107,23 +111,19 @@ class MLP(object):
         
         predicted_values = []
 
-        for t in range((np.shape(X)[0])):
-            for i in range(len(self.weights)):
-             if i == 0:
-                z_input = np.dot(self.weights[0],np.reshape(X[t,:],(X[t,:].shape[0],1)))+self.biases[0]
-                h_input2 = relu(z_input)
-             else: 
-                z_output = np.dot(self.weights[1],h_input2)+self.biases[1]
+        for t in range(np.shape(X)[0]):
+            
+            x = np.reshape(X[t,:],(X[t,:].shape[0],1)) #reshape the input into (784,1)
+             
+            z_input2 = np.dot(self.weights[0],x)+self.biases[0] 
+            h_input2 = relu(z_input2) #activation of the hidden layer
+ 
+            z_output = np.dot(self.weights[1],h_input2)+self.biases[1] 
 
+            predicted_values.append(z_output.argmax(axis=0)[0]) #prediction of the foward network and appending 
 
-            predicted_values.append((z_output.argmax(axis=0)).tolist())
-
-        #predicted_values = np.reshape(np.array(predicted_values), np.shape(np.shape(X)[0],))
         return predicted_values
 
-        #raise NotImplementedError
-#python hw1-q1.py mlp
-#C:\Users\rodol\Desktop\1ºAno MEEC\1º Semestre\2º Quarter\AProf\Laboratory\Homework 1\Deep-Learning-Homeworks\Homework 1
     def evaluate(self, X, y):
         """
         X (n_examples x n_features)  (97477, 784)
@@ -131,11 +131,12 @@ class MLP(object):
         """
         # Identical to LinearModel.evaluate()
         y_hat = self.predict(X)
-        #n_correct = (y[:,None] == y_hat).sum()
         n_correct = 0
-        for i in range(y.shape[0]):
-            if y[i,None] == y_hat[i]:
-                n_correct +=1
+
+        for i in range(y.shape[0]):   #For all the number of examples, check if the predictions are the same as the gold labels 
+            if y[i,None] == y_hat[i]: 
+                n_correct +=1         # if they're the same 
+        
         n_possible = y.shape[0]
         return n_correct / n_possible
 
@@ -148,25 +149,24 @@ class MLP(object):
         def relu(x):
             return np.maximum(0,x)
         
-        for t in range((np.shape(X)[0])): #per sample 
+        for t in range((np.shape(X)[0])): #for every observation of the possible 97477 
             
+            #Foward Network 
+            x = np.reshape(X[t,:],(X[t,:].shape[0],1)) #reshape the input into (784,1)
 
-            #Foward Network
-            for i in range(len(self.weights)):
-             if i == 0:
-                z_input = np.dot(self.weights[0],np.reshape(X[t,:],(X[t,:].shape[0],1)))+self.biases[0]
-                h_input = relu(z_input)
-                #np.maximum(z_input, np.zeros((np.shape(z_input)[0],1)))                 
-             else: 
-                z_output = np.dot(self.weights[1],h_input)+self.biases[1]
-                                  
-            m = np.max(z_output, axis = 0)
-            probs = np.exp(z_output-m) / np.sum(np.exp(z_output-m))
+            z_input = np.dot(self.weights[0],x)+self.biases[0]
+            h_input = relu(z_input) #activation of the hidden layer
 
+            z_output = np.dot(self.weights[1],h_input)+self.biases[1] #output layer before softmax
+
+            #Softmax activation                       
+            m = np.max(z_output, axis = 0) 
+            probs = np.exp(z_output-m) / np.sum(np.exp(z_output-m)) 
+
+            #Computation of the loss  
             y_one_hot = np.zeros((np.shape(z_output)[0],1))
             y_one_hot[y[t]] = 1
-            
-            loss.append(-np.transpose(y_one_hot).dot(np.log(probs + 10**-10)))
+            loss.append(-np.transpose(y_one_hot).dot(np.log(probs + 10**-10))) #loss
           
             
             #Backpropgation network
@@ -176,32 +176,36 @@ class MLP(object):
             num_layers = len(self.weights)
 
             for k in range(num_layers-1, -1, -1):
-             # Gradient of hidden parameters.
-                h = np.reshape(X[t,:],(X[t,:].shape[0],1)) if k == 0 else h_input #h1 = (1,200) h2= (784,)
-                grad_weights.append(grad_z.dot(np.transpose(h)))
-                grad_biases.append(grad_z)
-                # Gradient of hidden layer below.
-        # Gradient of hidden layer below before activation.
-               # if h_input == 0:
-                   # grad_z = 0
-                #else: 
-                grad_h = self.weights[k].T.dot(grad_z)
-                grad_z = grad_h #alterar isto
 
-        # Making gradient vectors have the correct order
+                h = x  if k == 0 else h_input    # x is the input of the layer
+
+                grad_weights.append(grad_z.dot(np.transpose(h)))     #append the changes to weights
+                grad_biases.append(grad_z)                           #append the changes to the biases 
+                
+                grad_h = self.weights[k].T.dot(grad_z) 
+                
+                drelu = h_input.copy()
+                drelu[drelu > 0] = 1  #derivative of the relu acitvation function
+                
+                if k == 1:  
+                    #the shape of grad_h changes from k = 1 to k = 0, (since we're changing layers), however the shape of drelu does not 
+                    #accordingly we must keep this if condition, otherwise there'll be a shape related error  
+                    
+                    grad_z = grad_h*drelu 
+
+        
+        # Making the gradient vectors have the correct order
             grad_weights.reverse()
             grad_biases.reverse()
+           
 
-            for m in range(num_layers):
+            for m in range(num_layers): #Stochastic gradient descent 
                 self.weights[m] -= learning_rate*grad_weights[m]
                 self.biases[m] -= learning_rate*grad_biases[m]
             
             
 
-        return np.sum(loss)
-
-
-        #raise NotImplementedError
+        return np.sum(loss)  #Sum of all the computed losses 
 
 
 def plot(epochs, train_accs, val_accs):
